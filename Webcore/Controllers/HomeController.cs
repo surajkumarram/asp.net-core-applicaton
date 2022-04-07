6922ba3aba86d86cf50e5cfc8f1a6854ac49723d
alisuhail085@gmail.com <alisuhail085@gmail.com>
2022-04-07 15:59:52 +0530

    Add project files.

---
+++ Webcore/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Webcore.Models;
+
+namespace Webcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.name = "This is View Bag";
+            ViewData["sad"] = "This is View Data";
+            TempData["tm"] = "This is Temp Data";
+
+            Emp obj = new Emp();
+            obj.Name = "Suraj Kumar";
+            obj.Course = "MCA";
+            obj.Address = "Noida";
+
+            ViewData["objemp"] = obj;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
